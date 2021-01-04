f98f153183653b708fcc90d44915d43a1b803d05
aroseca15 <69179642+aroseca15@users.noreply.github.com>
2021-01-04 15:07:22 -0700

    repaired heroku connection

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
+const mongodb_uri = process.env.MONGODB_URI ||"mongodb:27017/localhost/budget";
 const app = express();
 
 app.use(logger("dev"));
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(mongodb_uri, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
@@ -26,3 +26,4 @@ app.use(require("./routes/api.js"));
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
